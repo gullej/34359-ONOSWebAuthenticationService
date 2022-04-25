84c2140588721ac46501e5e70f3d7d908410f4cd
gullej <gustav@juul-jorgensen.com>
2022-04-25 10:53:44 +0200

    Working mininet

--- mn/mn_outsideConnection.py
+++ mn/mn_outsideConnection.py
@@ -10,45 +10,40 @@ from mininet.util import dumpNetConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
 from mininet.link import Intf
-from mininet.node import RemoteController
+from mininet.node import Controller, OVSKernelSwitch, RemoteController
+from mininet.log import setLogLevel, info
 
-class MyTopo(Topo):
-    def __init__(self, **opts):
-        Topo.__init__(self, **opts)
+def topology():
+    "Create a network."
+    net = Mininet(controller=RemoteController, switch=OVSKernelSwitch)
 
-	s1 = self.addSwitch('s1', cls = OVSSwitch, protocols='OpenFlow13')
-            
-   	h2 = self.addHost('h2', address="10.0.0.2")
-    	h3 = self.addHost('h3', address="10.0.0.3")
-    	
-    	self.addLink(s1, h2)
-    	self.addLink(s1, h3)
-        
-        intf_ = Intf("v2", node=s1) # s1.attach("mytap")
+    info("*** Creating nodes\n")
 
-class ONOSController (RemoteController):
-
-    def __init__ (self):
-        RemoteController.__init__(self,'ONOSController','127.0.0.1',6633)
-
-def setupNet():
-
-    topo = MyTopo()
-        
-    net = Mininet(topo=topo, controller=None, link=TCLink, listenPort=6634)
-   
-    c0 = ONOSController()
-    net.addController(c0)   
-
-    hosts = net.hosts
-
-    net.start()
-    CLI(net)
+    c1 = net.addController('c1', controller=RemoteController, ip="127.0.0.1", port=6653)
     
+    s1 = net.addSwitch('s1', cls = OVSSwitch, protocols='OpenFlow13')
+    h2 = net.addHost('h2', address="10.0.0.2")
+    h3 = net.addHost('h3', address="10.0.0.3")
+
+    info("*** Creating links\n")
+    net.addLink(s1, h2)
+    net.addLink(s1, h3)
+    intf_=Intf("v2", node=s1) # s1.attach("mytap")
+    
+   
+    info("*** Starting network\n")
+    net.build()
+    c1.start()
+    s1.start([c1])
+ 
+    info("*** Running CLI\n")
+    CLI(net)
+
+    info("*** Stopping network\n")
     net.stop()
 
-if __name__ == '__main__':
 
-    setLogLevel('info')   
-    setupNet()
+if __name__ == '__main__':
+    setLogLevel('info')
+    topology()
 
